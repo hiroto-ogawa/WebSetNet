b712e770fa5045b95e97f791eb3333fa5f76aaf5
hiroto-ogawa <hiroto.og12@gmail.com>
2022-10-07 19:14:42 +0900

    データ更新時刻表示

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// JSONを取得するURL
+// 不正なURL一覧が記されているJSONを取得するURL
 var url = "http://data.phishtank.com/data/online-valid.json";
 
 // chrome.local.storage の容量節約のため、URL から ドメインを抜き出す
@@ -14,12 +14,13 @@ const downloadJSON = (url) => {
         .then(data => {
             let array_datum = Array.from(data);
             let domains = new Set();
+            let date = new Date().toLocaleString('ja-JP');
             for (let i = 0; i < array_datum.length; i++) {
                 // domains.add(domainParser(array_datum[i].url));
                 domains.add(array_datum[i].url);
             }
 
-            chrome.storage.local.set({'urls': [...domains]});
+            chrome.storage.local.set({'urls': [...domains], 'last_update_date': date});
             console.log("保存完了");
         });
 }
@@ -42,6 +43,14 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
         case 'downloadInvalidURL':
             downloadJSON(url)
+            chrome.storage.local.get('last_update_date', (date) => {
+                sendResponse(date)
+            });
+            break
+        case 'getLastUpdateDate':
+            chrome.storage.local.get('last_update_date', (date) => {
+                sendResponse(date)
+            });
             break
     }
     return true
@@ -51,4 +60,5 @@ chrome.runtime.onInstalled.addListener(() => {
     downloadJSON(url)
 });
 
+// 3時間に一回JSONを取得
 setInterval(() => downloadJSON(url), 10800000)
--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
-function submit() {
-    chrome.runtime.sendMessage({name: "downloadInvalidURL"});
+function submit(name) {
+    chrome.runtime.sendMessage({name: name}, function(res){
+        document.querySelector("#updated_time").innerText = `Last Update Date: ${res.last_update_date || 'none'}`;
+    });
 }
 
-document.getElementById("btn").addEventListener("click", submit);
+document.getElementById("btn").addEventListener("click", submit("downloadInvalidURL"));
+
+window.onload = submit("getLastUpdateDate");
